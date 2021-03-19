57d48c47fc084ec50ca71e8413e106293d61f8ce
Enantiodromis <georgerebradley@gmail.com>
2021-03-19 11:45:03 +0000

    Plotting

--- txt_mining.py
+++ txt_mining.py
@@ -2,6 +2,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+from sklearn.feature_extraction.text import CountVectorizer
+import matplotlib.pyplot as plt
+
 
 # Reading in the text_data csv file as a df (vectorised)
 df = pd.read_csv('data/text_data/Corona_NLP_train.csv')
@@ -30,11 +33,14 @@ df.OriginalTweet = df.OriginalTweet.str.lower()\
 # Tokenize the tweets (i.e. convert each into a list of words) frequency of every word in the corpus
 token_count = df.OriginalTweet.str.split(expand = True).stack().value_counts()
 
-# Counting the total number of all words (including repetitions)
-print("Total number of all words (including repetitions): " + str(sum(token_count)))
+# Counting the total number of all words (including repetitions) and distinct words:
+def word_count(input_series):
+    all_total, distinct_total = str(sum(token_count)), str(len(token_count))
+    return all_total, distinct_total
 
-# Counting the number of distinct words
-print("Total number of distinct words: " + str(len(token_count)))
+all_words, distinct_words = word_count(token_count)
+print("Number of all words (including repetitions): %s" %all_words)
+print("Number of distinct words: %s" %distinct_words)
 # The 10 most frequent words in the corpus
 print(token_count.head(10))
 
@@ -43,7 +49,35 @@ df.OriginalTweet = df.OriginalTweet.apply(lambda x: [item for item in x.split() 
 
 # Frequency of every word in the corpus
 token_count = df.OriginalTweet.str.split(expand = True).stack().value_counts()
+all_words, distinct_words = word_count(token_count)
 
 # Counting the total number of all words (including repetitions)
-print("Total number of all words (including repetitions): " + str(sum(token_count)))
+print("Number of all words (including repetitions): %s" %all_words)
 print(token_count.head(10))
+
+# 1.3
+# Plot a histogram with word frequencies, where the horizontal axis corresponds to words, while the vertical axis indicates the fraction of documents in a which a word appears. 
+# The words should be sorted in increasing order of their frequencies. Use a line chart for this, instead of a histogram.
+words = set(df.OriginalTweet.str.split(expand = True).stack())
+document = df.OriginalTweet.tolist()
+#print(document)
+vectorizer =  CountVectorizer() # Creating a vectorizer object
+vectorizer.fit(document)
+# Printing the identified Unique words along with their indices 
+#print("Vocabulary: ", vectorizer.vocabulary_) 
+  
+# Encode the Document 
+vector = vectorizer.transform(document) 
+  
+# Summarizing the Encoded Texts 
+print("Encoded Document is:") 
+vector_summed = sorted(sum(vector.toarray()).tolist())
+print(len(vector_summed))
+
+y = range(1, len(vector_summed)+1)
+
+plt.plot(y,vector_summed)
+plt.title('Word frequency')
+plt.xlabel('WORDS')
+plt.ylabel('FREQUENCY')
+plt.show()
