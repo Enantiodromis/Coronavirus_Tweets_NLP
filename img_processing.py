f735bec34ea79bb008f0188ef2b67fee94bce0f8
Enantiodromis <georgerebradley@gmail.com>
2021-03-23 16:37:29 +0000

    report change

--- img_processing.py
+++ img_processing.py
@@ -21,10 +21,16 @@ bush_house = imageio.imread('data/image_data/bush_house_wikipedia.jpg')
 forestry = imageio.imread('data/image_data/forestry_commission_gov_uk.jpg')
 rolland = imageio.imread('data/image_data/rolland_garros_tv5monde.jpg')
 
-# 1.1 
+################
+# Question 2.1 #
+################
+
 # Determining the size of the avengers_imdb.jpg image
 avengers_shape = avengers.shape
 
+#####################
+# Formatting output #
+#####################
 table_1 = PrettyTable()
 table_1.title = 'Image Mining 1.1'
 table_1.field_names = ["Question", "Findings"]
@@ -32,41 +38,48 @@ table_1.add_row(['Size of avengers_imdb.jpg:',  avengers_shape])
 table_1.align = "l"
 print(table_1)
 
+# Creating a figure and subplots for image output formatting
 plt.figure(figsize=(10,5))
 plt.subplot(131)
 plt.title("Original image", fontsize = 8)
 plt.axis('off')
 plt.imshow(avengers)
 
-# Produce a grayscale representation
+# Producing a grayscale representation of the avengers image
 avengers_grayscale = color.rgb2gray(avengers)
 
+# Plotting
 plt.subplot(132)
 plt.title("Grayscale image", fontsize = 8)
 plt.axis('off')
 plt.imshow(avengers_grayscale, cmap=plt.cm.gray)
 
-# Produce a binary representation
+# Producing a binary representation of the avengers image
 threshold = filters.threshold_otsu(avengers_grayscale)
 avengers_binary = avengers_grayscale > threshold
 
+# Plotting
 plt.subplot(133)
 plt.title("Black-and-white image", fontsize = 8)
 plt.axis('off')
 plt.imshow(avengers_binary, cmap=plt.cm.gray)
 plt.savefig('outputs/avengers_all.jpg')
 
-# 1.2
-plt.figure(figsize=(10,3))
+################
+# Question 2.2 #
+################
 
+# Plotting
+plt.figure(figsize=(10,3))
 plt.subplot(141)
 plt.title("Original image", fontsize = 8)
 plt.axis('off')
 plt.imshow(bush_house)
 
-# Add Gaussian random noise to the bush_house_wikipedia.jpg image with variance 0.1
+# Adding Gaussian random noise to the bush_house_wikipedia.jpg image with variance 0.1
 bush_house_rn = random_noise(bush_house, mode='gaussian', var=0.1)
 
+# Plotting
 plt.subplot(142)
 plt.title("Gaussian random noise", fontsize = 8)
 plt.axis('off')
@@ -75,6 +88,7 @@ plt.imshow(bush_house_rn)
 # Filtering the perturbed image with a Gaussian mask sigma = 1
 bush_house_gaus_mask = gaussian_filter(bush_house_rn, sigma=1)
 
+# Plotting
 plt.subplot(143)
 plt.title("Gaussian mask", fontsize = 8)
 plt.axis('off')
@@ -83,47 +97,57 @@ plt.imshow(bush_house_gaus_mask)
 # Applying a uniform smoothing mask (9x9)
 bush_house_uniform = uniform_filter(bush_house_gaus_mask, size = 9)
 
+# Plotting
 plt.subplot(144)
 plt.title("Uniform smoothing mask", fontsize = 8)
 plt.axis('off')
 plt.imshow(bush_house_uniform)
 plt.savefig('outputs/bush_house_all.jpg')
 
-# 1.3
-# Divide the forest_commission_gov_uk.jpg into 5 segments using k-means segmentation
+################
+# Question 2.3 #
+################
+
+# Dividing the forest_commission_gov_uk.jpg into 5 segments using k-means segmentation
 forest_segmented = slic(forestry, n_segments=5, compactness=20.0, start_label=1)
 
+# Plotting
 plt.figure(figsize=(10,3))
-
 plt.subplot(121)
 plt.title("Original image", fontsize = 8)
 plt.axis('off')
 plt.imshow(forestry)
 
+# Plotting
 plt.subplot(122)
 plt.title("Segmented image", fontsize = 8)
 plt.axis('off')
 plt.imshow(forest_segmented)
 plt.savefig('outputs/forestry_all.jpg')
 
-# 1.4 
-#  Perform Canny edge detection and apply Hough transform on rolland_garros_tv5monde.jpg
+################
+# Question 2.4 #
+################
+
+#  Performing Canny edge detection and applying Hough transform on rolland_garros_tv5monde.jpg
 rolland_grayscale = color.rgb2gray(rolland)
 rolland_canny = canny(rolland_grayscale)
 lines = transform.probabilistic_hough_line(np.fliplr(rotate(rolland_canny, 180)), threshold=160, line_length=105, line_gap=10)
 
+# Plotting
 plt.figure(figsize=(10,3))
-
 plt.subplot(131)
 plt.title("Original image", fontsize = 8)
 plt.axis('off')
 plt.imshow(rolland)
 
+# Plotting
 plt.subplot(132)
 plt.title("Canny edge detection", fontsize = 8)
 plt.axis('off')
 plt.imshow(rolland_canny)
 
+# Plotting
 plt.subplot(133)
 plt.title("Hough transform", fontsize = 8)
 plt.axis('off')
--- txt_mining.py
+++ txt_mining.py
@@ -14,63 +14,69 @@ from prettytable import PrettyTable
 # Reading in the text_data csv file as a df (vectorised)
 df = pd.read_csv('data/text_data/Corona_NLP_train.csv')
 
-# 1.1
+################
+# Question 1.1 #
+################
+
 # Computing the possible sentiment values
 unique_sentiment_values = df.Sentiment.unique()
 #print(unique_sentiment_values)
 
-# The second most popular sentiment in the tweets  
+# Computing the second most popular sentiment in the tweets  
 sentiment_values_count =  df.Sentiment.value_counts()
 second_popular_sentiment = list(sentiment_values_count.items())[1][0]
 #print("The second most popular sentiment in the tweets is: " + second_popular_sentiment)
 
-# Date with the greatest number of extremely positive tweets
+# Retrieving the date with the greatest number of extremely positive tweets
 max_ep_tweets_date = df[df.Sentiment == 'Extremely Positive']
 max_ep_tweets_date_list = max_ep_tweets_date.groupby('TweetAt')['Sentiment'].value_counts().sort_values(ascending=False).reset_index(name = 'Counts')
 date_of_max_ep_tweets = max_ep_tweets_date_list['TweetAt'].iloc[0]
 #print("Date of the max amount of extremely positive: " + date_of_max_ep_tweets)
 
-# Converting the messages to lower case replace non-alphabetic characters with whitespaces and ensure that the words of a message are seperated by a single whitespace
+# Converting the messages to lower case replace non-alphabetic characters with whitespaces and ensuring that the words of a message are seperated by a single whitespace
 df['OriginalTweet_modified'] = df.OriginalTweet.str.lower()\
                                          .replace(to_replace = '(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})+', value = '', regex = True)\
                                          .replace(to_replace = '[^a-z]+', value = ' ', regex = True)\
                                          .replace(to_replace = '[\s\s]+', value = ' ', regex = True)\
 
-# 1.2
-# Tokenize the tweets (i.e. convert each into a list of words) frequency of every word in the corpus
+################
+# Question 1.2 #
+################
+
+# Tokenizing the tweets (i.e. convert each into a list of words) frequency of every word in the corpus
 token_count = df.OriginalTweet_modified.str.split(expand = True).stack().value_counts()
 
 # Counting the total number of all words (including repetitions) and distinct words:
 def word_count(input_series):
     all_total, distinct_total = str(sum(input_series)), str(len(input_series))
     return all_total, distinct_total
-
 all_words, distinct_words = word_count(token_count)
-#print("Number of all words (including repetitions): %s" %all_words)
-#print("Number of distinct words: %s" %distinct_words)
-# The 10 most frequent words in the corpus
-#print(token_count.head(10))
 
-# Remove stop words, words with ≤ 2 characters and recalculate the number of all words (including repetitions) and the 10 most frequent words in the modified corpus.
+# Retrieving the top ten most frequently used words
+top_ten = [word for word in token_count[:10].keys()]
+
+# Removing stop words, words with ≤ 2 characters and recalculating the number of all words (including repetitions) and the 10 most frequent words in the modified corpus.
 df.OriginalTweet_modified = df.OriginalTweet_modified.apply(lambda x: [item for item in x.split() if item not in ENGLISH_STOP_WORDS and len(item) > 2]).apply(lambda x: ' '.join(x))
 
-# Frequency of every word in the corpus
+# Frequency of every word in the corpus after stop words and words with ≤ 2 characters have been removed
 token_count_after = df.OriginalTweet_modified.str.split(expand = True).stack().value_counts()
 all_words_after, distinct_words_after = word_count(token_count_after)
 
-# Counting the total number of all words (including repetitions)
-#print("Number of all words (including repetitions): %s" %all_words_after)
-#print(token_count_after.head(10))
+# Retrieving the top ten most frequently used words
+top_ten_after = [word for word in token_count_after[:10].keys()]
 
-# 1.3
-# Plot a histogram with word frequencies, where the horizontal axis corresponds to words, while the vertical axis indicates the fraction of documents in a which a word appears. 
-# The words should be sorted in increasing order of their frequencies. Use a line chart for this, instead of a histogram.
+################
+# Question 1.3 #
+################
+
+# Plotting line chart with word frequencies, where the horizontal axis corresponds to words, while the vertical axis indicates the fraction of documents in which a word appears. 
+# The words are sorted in increasing order of their frequencies.
 document = df.OriginalTweet_modified.tolist()
 
-vectorizer =  CountVectorizer() # Creating a vectorizer object
+vectorizer =  CountVectorizer()
 vector = vectorizer.fit_transform(document) 
   
-# Summarizing the Encoded Texts 
+# Manipulating the returned data to produce data for plotting.
 vector_summed = sum(vector.toarray() > 0).tolist()
 words_and_sums = sorted(zip(vector_summed, vectorizer.get_feature_names()), key= lambda x : x[0])
 
@@ -83,6 +89,7 @@ ordered_frequencies_100 = [x[0]/len(df.OriginalTweet) for x in words_and_sums[-1
 ordered_words_total_len = len(ordered_frequencies_total)
 x_range_total = range(ordered_words_total_len)
 
+# Plotting
 plt.figure(figsize=(10,6))
 plt.plot(x_range_total,ordered_frequencies_total)
 plt.xticks(np.arange(0, ordered_words_total_len, 3000))
@@ -91,8 +98,9 @@ plt.tick_params(axis='x', which='major', labelsize=8)
 plt.title('Word Frequencies (entire dataset)')
 plt.xlabel('Words (word index)')
 plt.ylabel('Frequencies (fraction of documents in which the word appears)')
-plt.savefig('graphs/word_frequency.png')
+plt.savefig('outputs/word_frequency.png')
 
+# Plotting
 plt.figure(figsize=(10,6))
 plt.plot(ordered_words_100,ordered_frequencies_100)
 plt.xticks(rotation = 90)
@@ -100,14 +108,13 @@ plt.tick_params(axis='x', which='major', labelsize=6)
 plt.title('Word Frequencies (top 100 word frequencies)')
 plt.xlabel('Words')
 plt.ylabel('Frequencies (fraction of documents in which the word appears)')
-plt.savefig('graphs/word_frequency_100.png')
+plt.savefig('outputs/word_frequency_100.png')
 
-# 1.4
-# This task can be done individually from the previous three. 
-# Produce a Multinomial Naive Bayes classiﬁer for the Coronavirus Tweets NLP data set using scikit-learn. 
-# For this, store the corpus in a numpy array, produce a sparse representation of the term-document matrix with a CountVectorizer and build the model using this term-document matrix. 
-# What is the error rate of the classiﬁer? 
-# You may want to check the scikit-learn documentation for performing this task
+################
+# Question 1.4 #
+################
+
+# Producing a Multinomial Naive Bayes classiﬁer for the Coronavirus Tweets NLP data set using scikit-learn. 
 X = np.array(df.OriginalTweet_modified)
 y = np.array(df.Sentiment)
 
@@ -120,12 +127,15 @@ nb.fit(X, y)
 # Class predictions for X_train_tweet
 y_pred_class = nb.predict(X)
 
-# Calculate accuracy of class predictions
+# Calculate accuracy and error rate of class predictions
 accuracy = metrics.accuracy_score(y, y_pred_class)
 error_rate = str(round((1 - accuracy) * 100,2)) + "%"
-#print(error_rate)
 
-# FORMATTING OUTPUT USING PRETTYTABLE
+
+#######################################
+# FORMATTING OUTPUT USING PRETTYTABLE #
+#######################################
+
 # OUTPUT FOR 1.1
 table_1 = PrettyTable()
 table_1.title = 'Text Mining 1.1'
@@ -142,9 +152,9 @@ table_2.title = 'Text Mining 1.2'
 table_2.field_names = ["Question", "Findings"]
 table_2.add_row(['Total number of all words (including repetitions):',  all_words])
 table_2.add_row(['Total number of all distinct words:',  distinct_words])
-table_2.add_row(['10 most frequent words in the corpus:', [word for word in token_count[:10].keys()]])
+table_2.add_row(['10 most frequent words in the corpus:', top_ten])
 table_2.add_row(['Total number of all words (including repetitions) (After stop words and words with <= 2 characters have been removed):',  all_words_after])
-table_2.add_row(['10 most frequent words in the corpus (After stop words and words with <= 2 characters have been removed):', [word for word in token_count_after[:10].keys()]])
+table_2.add_row(['10 most frequent words in the corpus (After stop words and words with <= 2 characters have been removed):', top_ten_after])
 table_2.align = "l"
 print(table_2)
 
