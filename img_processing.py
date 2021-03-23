8a2d44ad2398d28485844f3ecd85eb09077bedac
Enantiodromis <georgerebradley@gmail.com>
2021-03-23 11:58:47 +0000

    Image formatting

--- img_processing.py
+++ img_processing.py
@@ -24,50 +24,92 @@ rolland = imageio.imread('data/image_data/rolland_garros_tv5monde.jpg')
 # Determining the size of the avengers_imdb.jpg image
 print(avengers.shape)
 
+plt.figure(figsize=(10,5))
+plt.subplot(131)
+plt.title("Original image", fontsize = 8)
+plt.axis('off')
+plt.imshow(avengers)
+
 # Produce a grayscale representation
 avengers_grayscale = color.rgb2gray(avengers)
+plt.subplot(132)
+plt.title("Grayscale image", fontsize = 8)
 plt.axis('off')
 plt.imshow(avengers_grayscale, cmap=plt.cm.gray)
-plt.savefig('outputs/avengers_grayscale.jpg')
 
 # Produce a binary representation
 threshold = filters.threshold_otsu(avengers_grayscale)
 print('Otsu method threshold = ', threshold)
 avengers_binary = avengers_grayscale > threshold
+plt.subplot(133)
+plt.title("Black-and-white image", fontsize = 8)
+plt.axis('off')
 plt.imshow(avengers_binary, cmap=plt.cm.gray)
-plt.savefig('outputs/avengers_binary.jpg')
+plt.savefig('outputs/avengers_all.jpg')
 
 # 1.2
+plt.figure(figsize=(10,3))
+plt.subplot(141)
+plt.title("Original image", fontsize = 8)
+plt.axis('off')
+plt.imshow(bush_house)
+
 # Add Gaussian random noise to the bush_house_wikipedia.jpg image with variance 0.1
 bush_house_rn = random_noise(bush_house, mode='gaussian', var=0.1)
+plt.subplot(142)
+plt.title("Gaussian random noise", fontsize = 8)
+plt.axis('off')
+plt.imshow(bush_house_rn)
 
 # Filtering the perturbed image with a Gaussian mask sigma = 1
 bush_house_gaus_mask = gaussian_filter(bush_house_rn, sigma=1)
+plt.subplot(143)
+plt.title("Gaussian mask", fontsize = 8)
+plt.axis('off')
 plt.imshow(bush_house_gaus_mask)
-plt.savefig('outputs/bush_house_gaus_mask.jpg')
-
 
 # Applying a uniform smoothing mask (9x9)
 bush_house_uniform = uniform_filter(bush_house_gaus_mask, size = 9)
+plt.subplot(144)
+plt.title("Uniform smoothing mask", fontsize = 8)
+plt.axis('off')
 plt.imshow(bush_house_uniform)
-plt.savefig('outputs/bush_house_uniform.jpg')
+plt.savefig('outputs/bush_house_all.jpg')
 
 # 1.3
 # Divide the forest_commission_gov_uk.jpg into 5 segments using k-means segmentation
 forest_segmented = slic(forestry, n_segments=5, compactness=20.0, start_label=1)
+
+plt.figure(figsize=(10,3))
+plt.subplot(121)
+plt.title("Original image", fontsize = 8)
+plt.axis('off')
+plt.imshow(forestry)
+plt.subplot(122)
+plt.title("Segmented image", fontsize = 8)
+plt.axis('off')
 plt.imshow(forest_segmented)
-plt.savefig('outputs/forest_segmented.jpg')
+plt.savefig('outputs/forestry_all.jpg')
 
 # 1.4 
 #  Perform Canny edge detection and apply Hough transform on rolland_garros_tv5monde.jpg
 rolland_grayscale = color.rgb2gray(rolland)
 rolland_canny = canny(rolland_grayscale)
-plt.imshow(rolland_canny)
-
 lines = transform.probabilistic_hough_line(np.fliplr(rotate(rolland_canny, 180)), threshold=160, line_length=105, line_gap=10)
-plt.figure()
+
+plt.figure(figsize=(10,3))
+plt.subplot(131)
+plt.title("Original image", fontsize = 8)
+plt.axis('off')
+plt.imshow(rolland)
+plt.subplot(132)
+plt.title("Canny edge detection", fontsize = 8)
+plt.axis('off')
+plt.imshow(rolland_canny)
+plt.subplot(133)
+plt.title("Hough transform", fontsize = 8)
+plt.axis('off')
 for line in lines:
     p0, p1 = line
     plt.plot((p0[0], p1[0]), (p0[1], p1[1]))
-plt.savefig('outputs/probabilistic_hough_line.jpg')
-
+plt.savefig('outputs/rolland_all.jpg')
